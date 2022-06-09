b4c2545a4d2a001b6cf3affa5d0dfa6fd0209a37
CPuPush <fpakpahan44@gmail.com>
2022-06-10 01:59:37 +0700

    LOGREG

--- app/src/main/java/com/example/expensetrackersystem/Register.java
+++ app/src/main/java/com/example/expensetrackersystem/Register.java
@@ -1,13 +1,13 @@
-package com.example.expensetrackersystem.fragments;
-import com.example.expensetrackersystem.R;
-
+package com.example.expensetrackersystem;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
@@ -21,46 +21,54 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-
-    private EditText etEmail, etPassword;
-    private String email, password;
-    private String URL = "http://10.0.2.2/login/login.php";
+public class Register extends AppCompatActivity {
+    private EditText etName, etEmail, etPassword, etReenterPassword;
+    private TextView tvStatus;
+    private Button btnRegister;
+    private String URL = "http://10.0.2.2/login/register.php";
+    private String name, email, password, reenterPassword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        email = password = "";
+        setContentView(R.layout.activity_register);
+        etName = findViewById(R.id.etName);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+        etReenterPassword = findViewById(R.id.etReenterPassword);
+        tvStatus = findViewById(R.id.tvStatus);
+        btnRegister = findViewById(R.id.btnRegister);
+        name = email = password = reenterPassword = "";
     }
 
-    public void login(View view) {
+    public void save(View view) {
+        name = etName.getText().toString().trim();
         email = etEmail.getText().toString().trim();
         password = etPassword.getText().toString().trim();
-        if(!email.equals("") && !password.equals("")){
+        reenterPassword = etReenterPassword.getText().toString().trim();
+        if(!password.equals(reenterPassword)){
+            Toast.makeText(this, "Password Mismatch", Toast.LENGTH_SHORT).show();
+        }
+        else if(!name.equals("") && !email.equals("") && !password.equals("")){
             StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    Log.d("res", response);
                     if (response.equals("success")) {
-                        Intent intent = new Intent(MainActivity.this, Success.class);
-                        startActivity(intent);
-                        finish();
+                        tvStatus.setText("Successfully registered.");
+                        btnRegister.setClickable(false);
                     } else if (response.equals("failure")) {
-                        Toast.makeText(MainActivity.this, "Invalid Login Id/Password", Toast.LENGTH_SHORT).show();
-                    }
+                        tvStatus.setText("Something went wrong!");                    }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
                 }
             }){
                 @Override
                 protected Map<String, String> getParams() throws AuthFailureError {
                     Map<String, String> data = new HashMap<>();
+                    data.put("name", name);
                     data.put("email", email);
                     data.put("password", password);
                     return data;
@@ -68,13 +76,11 @@ public class MainActivity extends AppCompatActivity {
             };
             RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
             requestQueue.add(stringRequest);
-        }else{
-            Toast.makeText(this, "Fields can not be empty!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void register(View view) {
-        Intent intent = new Intent(this, Register.class);
+    public void login(View view) {
+        Intent intent = new Intent(this, Login.class);
         startActivity(intent);
         finish();
     }
